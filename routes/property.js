18e7ff97c12b0d9f8972b0228c19d090ea5401bc
Mehul Sahu <mehulyashams@gmail.com>
2023-08-10 14:34:24 +0530

    feat(model,routes added),functions added in index

---
+++ routes/property.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const Property = require("../model/property");
+const propertyRouter = express.Router();
+
+
+propertyRouter.get("/v1/",(req,res)=>{
+    res.status(200).json({
+        message : "hello world"
+    })
+})
+
+propertyRouter.post("/v1/",(req,res)=>{
+
+})
+
+propertyRouter.put("/v1/update/:id",(req,res)=>{
+
+})
+
+propertyRouter.delete("/v1/:id",(req,res)=>{
+
+})
+
+module.exports = propertyRouter;
