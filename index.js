035b827c5b207b1fe023743bcff61a068d6141c0
yadavsatyapal1999 <114231518+yadavsatyapal1999@users.noreply.github.com>
2023-08-11 00:13:38 +0530

    env & git ignore modified in model property,js renamed with property_Schema.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -6,6 +7,7 @@ const userRouter = require("./routes/user");
 require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose.connect(process.env.MONGO_DB).then(res=>{
     console.log("Connected to Database Successfully");
@@ -13,6 +15,33 @@ mongoose.connect(process.env.MONGO_DB).then(res=>{
     console.log("Connection failed");
 })
 
+
 app.use("/prop",propertyRouter);
 app.use("/user",userRouter);
 app.listen(process.env.PORT);
+/*
+
+
+// Satyapal Code
+
+const express = require('express');
+const bodyParser= require('body-parser');
+const mongoose = require('mongoose');
+const addproperty =  require('./Routes_Satyapal/property')
+require('dotenv').config()
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+mongoose.connect(process.env.MONGO_DB).then(res=>{
+    console.log("Connected to DB");
+})
+.catch(err=>{
+    console.log("could not connect to DB");
+})
+app.use('/property',addproperty);
+
+app.listen(process.env.PORT,()=>{
+    console.log(`App is listening at ${process.env.PORT}`)
+})
+*/
