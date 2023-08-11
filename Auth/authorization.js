38cbfd0bb8da6ab176034bcf6698af5a28d32141
yadavsatyapal1999 <114231518+yadavsatyapal1999@users.noreply.github.com>
2023-08-11 13:48:24 +0530

    console.log removed from auth

--- Auth/authorization.js
+++ Auth/authorization.js
@@ -4,11 +4,10 @@ require('dotenv').config();
 const auth = (req, res, next) => {
 
     try {
-        console.log("auth")
+
         const token = req.headers.authorization // It will look for token in headers
         const verify = jwt.verify(token, process.env.SECRET_KEY); // verify token
         req.id = verify.id   // store user ref id
-        console.log(req.id)
         next();
     }
     catch {
