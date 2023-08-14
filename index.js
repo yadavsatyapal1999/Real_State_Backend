fc8df678b3433090d6a5203e7b1cd3a81d256892
yadavsatyapal1999 <114231518+yadavsatyapal1999@users.noreply.github.com>
2023-08-14 18:12:28 +0530

    Bank_loan corrected

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const propertyRouter = require("./routes/property");
 const userRouter = require("./routes/user");
 require('dotenv').config();
 const app = express();
+app.use(cors());
+app.use(express.static("uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,29 +22,3 @@ mongoose.connect(process.env.MONGO_DB).then(res=>{
 app.use("/prop",propertyRouter);
 app.use("/user",userRouter);
 app.listen(process.env.PORT);
-/*
-
-
-// Satyapal Code
-
-const express = require('express');
-const bodyParser= require('body-parser');
-const mongoose = require('mongoose');
-const addproperty =  require('./Routes_Satyapal/property')
-require('dotenv').config()
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
-mongoose.connect(process.env.MONGO_DB).then(res=>{
-    console.log("Connected to DB");
-})
-.catch(err=>{
-    console.log("could not connect to DB");
-})
-app.use('/property',addproperty);
-
-app.listen(process.env.PORT,()=>{
-    console.log(`App is listening at ${process.env.PORT}`)
-})
-*/
